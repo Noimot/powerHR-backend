8ba5da7ea68c9dbcd186aa0ba277325be92451b6
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-05-05 17:37:43 +0100

    changes updated

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,28 +1,22 @@
- import { Pool } from "pg"
- import dotenv from "dotenv"
+import pkg from 'pg';
+import dotenv from "dotenv"
 
- dotenv.config();
+const { Pool }  = pkg;
 
- const mydatabase = process.env.DATABASE_URL;
+dotenv.config();
 
-
-//  const pool = new Pool({
-//      user: "dbuser",
-//      host: "database.server.com",
-//      database: "mydb",
-//      password: "123456",
-//      port: 5432,
-//  });
-
- const connect = new Pool({
-     connectionString: mydatabase,
-     ssl: { rejectUnauthorized: false }
- });
-
- connect.on('error', (err, client) => {
-     console.error('Error:', err);
- });
+const mydatabase = process.env.DATABASE_URL;
 
 
- export default connect;
+const connect = new Pool({
+    connectionString: mydatabase,
+    ssl: { rejectUnauthorized: false }
+});
+
+connect.on('error', (err, client) => {
+    console.error('Error:', err);
+});
+
+
+export default connect;
 
