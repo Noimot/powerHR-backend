bb68f877597702d038a59dcb423607b4bd7e3de9
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-05-05 15:03:48 +0100

    changes updated

--- server/routes/employeeList.js
+++ server/routes/employeeList.js
@@ -1,9 +1,12 @@
-import { Ruoter } from 'express'
+import { Router } from 'express'
+import AuthController from '../controllers/AuthController.js'
+
 
 
 const employeeRouter = Router();
 
 employeeRouter.post('/employee',
+AuthController.employeeList
 )
 
 
