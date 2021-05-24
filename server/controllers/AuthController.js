77a05626da0b36222059725f1722f05a5b4b3531
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-05-24 12:33:38 +0100

    token added

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,14 +1,40 @@
 import connect from '../database/connect.js'
 
-class AuthController{
-    static userLogin(req, res){
-        res.status(200).json({
-            message: 'login successful',
-            body: req.body
-        })
+class AuthController {
+
+    static userLogin(req, res) {
+        const adminPassword = process.env.ADMIN_PASSWORD;
+        if (adminPassword) {
+            const tokenData = {
+                email,
+                role: result[0].role,
+                firstName: result[0].firstName,
+                lastName: result[0].lastName,
+                userId: result[0].userid,
+                expiryTime: "500h"
+            };
+            const token = Token.generateToken(tokenData);
+            return res.status(200).json({
+                status: "success",
+                statusCode: 200,
+                message: "login successful",
+                token,
+            });
+        } else {
+            return res.status(400).json({
+                status: "error",
+                statusCode: 400,
+                message: "Email or Password is wrong",
+            });
+        }
+
+        // res.status(200).json({
+        //     message: 'login successful',
+        //     body: req.body
+        // })
     }
 
-    static userSignup(req, res){
+    static userSignup(req, res) {
         res.status(200).json({
             status: 'successful',
             message: 'welcome to power-hr',
@@ -16,7 +42,7 @@ class AuthController{
         })
     }
 
-    static adminName(req, res){
+    static adminName(req, res) {
         res.status(200).json({
             status: 'successful',
             message: 'welcome to power-hr',
@@ -24,6 +50,60 @@ class AuthController{
         })
     }
 
+    static userLogin(req, res) {
+        const { email, password } = req.body;
+
+        connect.query(
+            `SELECT * FROM users WHERE email = '${email}'`,
+            (err, response) => {
+                console.log(err, "err");
+                console.log(response, "result");
+                const result = JSON.parse(JSON.stringify(response.rows));
+                console.log(result, "result");
+                console.log(result[0], "main result");
+
+                if (result.length > 0) {
+                    const checkPassword = bcrypt.compareSync(
+                        password,
+                        result[0].password
+                    );
+
+                    if (checkPassword) {
+                        const tokenData = {
+                            email,
+                            role: result[0].role,
+                            firstName: result[0].firstName,
+                            lastName: result[0].lastName,
+                            userId: result[0].userid,
+                            expiryTime: "500h"
+                        };
+                        const token = Token.generateToken(tokenData);
+                        return res.status(200).json({
+                            status: "success",
+                            statusCode: 200,
+                            message: "login successful",
+                            token,
+                        });
+                    } else {
+                        return res.status(400).json({
+                            status: "error",
+                            statusCode: 400,
+                            message: "Email or Password is wrong",
+                        });
+                    }
+
+                } else {
+                    return res.status(400).json({
+                        status: "error",
+                        statusCode: 400,
+                        message: "Email or Password is wrong",
+                    });
+                }
+            }
+        );
+    }
+
 }
 
 export default AuthController;
+
