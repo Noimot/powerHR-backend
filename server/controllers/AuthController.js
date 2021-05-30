99fda52494cb62480ae154d44a88cfb15193b503
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-05-30 11:57:13 +0100

    changes commited

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -13,73 +13,49 @@ class AuthController {
         const adminPassword = process.env.ADMIN_PASSWORD;
         const adminEmail = process.env.ADMIN_EMAIL;
         const adminId = process.env.ADMIN_ID_NUMBER;
-        // const { company_email, password } = req.body;
 
-        // connect.query(
-        //     `SELECT password, role, first_name, last_name, company_email, userid FROM add_employee`,
-        //     (err, response) => {
-        //         console.log(err, "err");
-                // console.log(response, "result");
-                // const result = JSON.parse(JSON.stringify(response.rows));
-                // console.log(result, "result");
-                // console.log(result[12], "main result");
+        if (adminPassword) {
+            const tokenData = {
+                role: 'admin',
+                email: adminEmail,
+                adminId,
+                expiryTime: "500h"
+            };
+            const token = Token.generateToken(tokenData);
+            return res.status(200).json({
+                status: "success",
+                statusCode: 200,
+                message: "login successful",
+                token,
+            });
+        }
+        else {
+            res.status(400).json({
+                status: "error",
+                statusCode: 400,
+                message: "Email or Password is wrong",
+            });
+        }
+    }
 
-                // if (result.length > 0) {
-                //     const checkPassword = bcrypt.compareSync(
-                //         password,
-                //         result[12].password
-                //     );
-
-                    if (adminPassword) {
-                        const tokenData = {
-                            // company_email,
-                            // role: result[12].role,
-                            // first_name: result[12].first_name,
-                            // last_name: result[12].last_name,
-                            // userid: result[12].userid,
-                            email: adminEmail,
-                            adminId,
-                            expiryTime: "500h"
-                        };
-                        const token = Token.generateToken(tokenData);
-                        return res.status(200).json({
-                            status: "success",
-                            statusCode: 200,
-                            message: "login successful",
-                            token,
-                        });}
-                    // } else {
-                    //      res.status(400).json({
-                    //         status: "error",
-                    //         statusCode: 400,
-                    //         message: "Email or Password is wrong",
-                    //     });
-                    // }
-
-                    // res.status(200).json({
-                    //     message: 'login successful',
-                    //     body: req.body
-                    // })
-                }
-            
 
     static userSignup(req, res) {
-            res.status(200).json({
-                status: 'successful',
-                message: 'welcome to power-hr',
-                body: req.body
-            })
-        }
+        res.status(200).json({
+            status: 'successful',
+            message: 'welcome to power-hr',
+            body: req.body
+        })
+    }
 
     static adminName(req, res) {
-            res.status(200).json({
-                status: 'successful',
-                message: 'welcome to power-hr',
-                name: process.env.ADMIN_NAME
-            })
-        }
+        res.status(200).json({
+            status: 'successful',
+            message: 'welcome to power-hr',
+            name: process.env.ADMIN_NAME
+        })
+    }
 
 }
 
-    export default AuthController;
+export default AuthController;
 
--- server/middleware/CheckConflicts.js
+++ server/middleware/CheckConflicts.js
@@ -3,67 +3,48 @@ import bcrypt from "bcryptjs"
 
 
 class CheckConflicts {
-    static validateUserDetails(req, res, next){
-        const password = req.body.password;
-        if(password.length < 8) {
-            return res.status(400).json({
-                status:'error',
-                message:'password length is less than 8'
-            })
-        }
-        next();
+  static validateUserDetails(req, res, next) {
+    const password = req.body.password;
+    if (password.length < 8) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'password length is less than 8'
+      })
     }
+    next();
+  }
 
 
-    static authenticateUserDetails(req, res, next){
-        const { password, adminId, email} = req.body;
+  static authenticateUserDetails(req, res, next) {
+    const { password, adminId, email } = req.body;
 
-        // const { password, adminId, email, company_email, userid } = req.body;
+    if (password !== process.env.ADMIN_PASSWORD && adminId !== process.env.ADMIN_ID_NUMBER || password !== process.env.ADMIN_PASSWORD && email !== process.env.ADMIN_EMAIL) {
+      return res.status(401).json({
+        message: 'incorrect password or id'
+      })
+    }
+    next();
+  }
 
-        // connect.query(
-        //   `SELECT password, role, first_name, last_name, company_email, userid FROM add_employee`,
-        //   (err, response) => {
-        //       // console.log(err, "err");
-        //       // console.log(response, "result");
-        //       const result = JSON.parse(JSON.stringify(response.rows));
-        //       // console.log(result, "result");
-        //       console.log(result[12], "main result");
-
-        //       if (result.length > 0) {
-        //         const hashedPassword = bcrypt.compareSync(
-        //           password,
-        //           result[12].password
-        //         );
-                // if(!password && adminId !== process.env.ADMIN_ID_NUMBER || password !== process.env.ADMIN_PASSWORD && email !== process.env.ADMIN_EMAIL || company_email !== result[12].company_email && password !== result[12].password || userid !== result[12].userid && password !== result[12].password ){
-
-        if(password !== process.env.ADMIN_PASSWORD && adminId !== process.env.ADMIN_ID_NUMBER || password !== process.env.ADMIN_PASSWORD && email !== process.env.ADMIN_EMAIL){
-            return res.status(401).json({
-                message: 'incorrect password or id'
-            })
-        }
-      
   
+  static existingUser(req, res, next) {
+    const { company_email, personal_email } = req.body;
+    connect.query(
+      `SELECT company_email, personal_email FROM add_employee WHERE company_email='${company_email}'`,
+      (err, response) => {
+        console.log(err, "err");
+        const result = JSON.parse(JSON.stringify(response.rows));
+        if (result.length > 0) {
+          return res.status(409).json({
+            status: "error",
+            message: "email address already exist",
+          });
+        }
         next();
-          }
-
-    static existingUser(req, res, next) {
-        const { company_email, personal_email } = req.body;
-        connect.query(
-          `SELECT company_email, personal_email FROM add_employee WHERE company_email='${company_email}'`,
-          (err, response) => {
-            console.log(err, "err");
-            const result = JSON.parse(JSON.stringify(response.rows));
-            if (result.length > 0) {
-              return res.status(409).json({
-                status: "error",
-                message: "email address already exist",
-              });
-            }
-            next();
-          }
-        );
       }
-    
+    );
+  }
+
 }
 
 export default CheckConflicts;
