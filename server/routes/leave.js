1834f0af586d71d6b84a6d3a7fbdaa733d33285c
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-06-05 18:55:07 +0100

    changes commited

--- server/routes/leave.js
+++ server/routes/leave.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import LeaveController from '../controllers/LeaveController.js'
+import CheckConflicts from '../middleware/CheckConflicts.js'
 
 const leaveRouter = Router();
 
 leaveRouter.post('/new',
+CheckConflicts.checkUserExistence,
 LeaveController.leaveRequest
 )
 
