7485fa972c6e47fc002f6b47d2a473fff8908cf3
Doyinsola24 <62024110+Doyinsola24@users.noreply.github.com>
2021-06-13 11:30:02 +0100

    changes commited

--- server/controllers/MemoController.js
+++ server/controllers/MemoController.js
@@ -2,9 +2,9 @@
 
  class MemoController {
      static addMemo (req, res) {
-         const  {category, subject, memos } = req.body;
+         const  {permission, category, subject, memos } = req.body;
          connect.query(
-             `INSERT INTO memo (category, subject, memos) VALUES ( '${category}', '${subject}', '${memos}' )`,
+             `INSERT INTO memo (permission, category, subject, memos) VALUES ( '${permission}', '${category}', '${subject}', '${memos}' )`,
              (err, response) => {
                  if (err) return ({ message: 'there is an error' })
                  const result = JSON.parse(JSON.stringify(response.rows))
